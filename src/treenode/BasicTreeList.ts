725263a3a0ee2f4e9c31097c3e512dec4341e220
Aaron Faanes <dafrito@gmail.com>
2021-11-06 03:43:32 -0500

    BasicTreeList supports direction and alignment

--- src/treenode/BasicTreeList.ts
+++ src/treenode/BasicTreeList.ts
@@ -1,12 +1,15 @@
 import WindowNode from "../WindowNode";
 import TreeNode from "./TreeNode";
 import AbstractTreeList from "./AbstractTreeList";
-import { SHRINK_SCALE } from "parsegraph-layout";
-import Direction, { NodePalette } from "parsegraph-direction";
+import { Alignment, SHRINK_SCALE } from "parsegraph-layout";
+import Direction, { NodePalette, turnRight } from "parsegraph-direction";
 
 export default class BasicTreeList extends AbstractTreeList {
   _lastRow: WindowNode;
   _palette: NodePalette<WindowNode>;
+
+  _direction: Direction;
+  _align: Alignment;
 
   constructor(
     title: TreeNode,
@@ -18,6 +21,18 @@ export default class BasicTreeList extends AbstractTreeList {
       throw new Error("Palette must be given");
     }
     this._palette = palette;
+    this._direction = Direction.FORWARD;
+    this._align = Alignment.NONE;
+  }
+
+  setAlignment(align: Alignment) {
+    this._align = align;
+    this.invalidate();
+  }
+
+  setDirection(dir: Direction) {
+    this._direction = dir;
+    this.invalidate();
   }
 
   connectSpecial(): WindowNode {
@@ -25,7 +40,8 @@ export default class BasicTreeList extends AbstractTreeList {
   }
 
   connectInitialChild(root: WindowNode, child: WindowNode): WindowNode {
-    root.connectNode(Direction.FORWARD, child);
+    root.connectNode(this._direction, child);
+    root.setNodeAlignmentMode(this._direction, this._align);
     child.setScale(SHRINK_SCALE);
     this._lastRow = root;
     return root;
@@ -33,8 +49,8 @@ export default class BasicTreeList extends AbstractTreeList {
 
   connectChild(lastChild: WindowNode, child: WindowNode): WindowNode {
     const bud = this._palette.spawn();
-    lastChild.connectNode(Direction.DOWNWARD, bud);
-    bud.connectNode(Direction.FORWARD, child);
+    lastChild.connectNode(turnRight(this._direction), bud);
+    bud.connectNode(this._direction, child);
     this._lastRow = bud;
     return bud;
   }
