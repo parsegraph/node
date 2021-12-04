b2f27b50e50f22e149c8527a38197388d9cbda92
Aaron Faanes <dafrito@gmail.com>
2021-12-03 19:39:28 -0600

    Audio-ify build

--- tmuxify.d/01-build.sh
+++ tmuxify.d/01-build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 while true; do
+    aplay ./audio/parsegraph-node-build-started.wav &
+    (npm run build && aplay ./audio/parsegraph-node-build-successful.wav) || aplay ./audio/parsegraph-node-build-failed.wav
     make build SITE_URL=$SITE_URL SITE_PORT=$SITE_PORT &
     serverpid=$!
     inotifywait -e modify -r package*.json tsconfig.json .babelrc webpack* src
